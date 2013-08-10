fea11b8ba2980a95a4e0226c387df668ddf65a82
benmliang <benmliang@gmail.com>
2013-08-10 14:40:58 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fieldr::Application.config.secret_key_base = '65ad55602deefa1786f6e0bcf2b7025b438fd022288cd61f1a1df0a8d2f6ddf10f00a063d4424c5fb9c4e5d0aab05490b384faa304b47855b0a0b5a796a4c02a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fieldr::Application.config.session_store :cookie_store, key: '_fieldr_session'
